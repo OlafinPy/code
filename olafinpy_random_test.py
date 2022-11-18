591feb34d43c63e6a6f8b86817e69debf6a2184e
OlafinPy <pasztor.lajos@gmail.com>
2022-11-18 12:58:32 +0100

    v1.0

---
+++ olafinpy_random_test.py
@@ -0,0 +1,8 @@
+import random
+n=int(input("How many random number do you need?"))
+k=int(input("Wich number can be the biggest?"))
+x=[]
+i=0
+for i in range(n):
+    x.append(random.randint(0,k))
+print(x)    
